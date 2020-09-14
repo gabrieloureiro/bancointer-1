9d0ecc779623dbe4ace74b714be7506901bf68f2
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-14 17:09:35 -0300

    ⚡  chore(mobile): add style guide

---
+++ mobile/src/screens/Login/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Login = (): JSX.Element => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text>Ahhh ... Hellow???</Text>
+    </View>
+  );
+};
+
+export default Login;
