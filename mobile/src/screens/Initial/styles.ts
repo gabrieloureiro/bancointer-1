e4558f2b0201594f7add3e3e64e1f7af33cf7b28
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-10-31 15:12:20 -0300

    ⚡  chore: remove unused styles

--- mobile/src/screens/Initial/styles.ts
+++ mobile/src/screens/Initial/styles.ts
@@ -21,12 +21,6 @@ export const Cards = styled.View`
   margin-top: -25px;
 `;
 
-export const CardLogin = styled.View`
-  background: ${props => props.theme.colors.light_gray_bg};
-  padding: 26px 16px 16px;
-  border-radius: 8px;
-`;
-
 export const Card = styled.View`
   background: ${props => props.theme.colors.light_gray_bg};
   margin-top: 16px;
@@ -40,76 +34,6 @@ export const Card = styled.View`
 export const Wrapper = styled.View`
   flex-direction: row;
   align-items: center;
-`;
-
-export const Avatar = styled.Image`
-  width: 50px;
-  height: 50px;
-  border-radius: 100px;
-  margin-right: 16px;
-  flex-shrink: 0;
-`;
-
-export const Info = styled.View`
-  margin-right: 14px;
-  flex: 1;
-`;
-
-export const InfoName = styled.Text.attrs(() => ({
-  numberOfLines: 2,
-}))`
-  text-transform: uppercase;
-  font-family: ${props => props.theme.fonts.medium};
-  color: ${props => props.theme.colors.text_base};
-  font-size: 15px;
-`;
-
-export const InfoAgency = styled.Text`
-  font-family: ${props => props.theme.fonts.regular};
-  color: ${props => props.theme.colors.text_complement};
-  font-size: 15px;
-`;
-
-export const ChangeButton = styled.TouchableOpacity`
-  width: 100px;
-  height: 32px;
-  align-items: center;
-  justify-content: center;
-  border-radius: 4px;
-  flex-shrink: 0;
-  border: 1px solid ${props => props.theme.colors.light_gray_complement};
-`;
-
-export const ChangeButtonTxt = styled.Text`
-  font-family: ${props => props.theme.fonts.bold};
-  color: ${props => props.theme.colors.text_base};
-  font-size: 14px;
-  text-transform: uppercase;
-`;
-
-export const LoginButton = styled.TouchableOpacity`
-  margin: 35px auto 0;
-  border-radius: 4px;
-  overflow: hidden;
-  height: 48px;
-  width: 220px;
-`;
-
-export const LoginButtonBg = styled(LinearGradient).attrs(props => ({
-  colors: [props.theme.colors.primary_darken, props.theme.colors.primary_light],
-  start: [0, 0],
-  end: [1, 0],
-}))`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const LoginButtonTxt = styled.Text`
-  font-family: ${props => props.theme.fonts.semi_bold};
-  color: ${props => props.theme.colors.white};
-  text-transform: uppercase;
-  font-size: 16px;
 `;
 
 export const IconWrapper = styled.TouchableOpacity`
