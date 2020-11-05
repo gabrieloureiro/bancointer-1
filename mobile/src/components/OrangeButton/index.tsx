cf412f8508f92f765980791080074f1d5788efed
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-11-04 23:50:33 -0200

    ⚡  chore: add isLoading prop to orange button component

--- mobile/src/components/OrangeButton/index.tsx
+++ mobile/src/components/OrangeButton/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { RectButtonProperties } from 'react-native-gesture-handler';
+
+import { ActivityIndicator } from 'react-native';
+import { ThemeContext } from 'styled-components';
 
 import * as S from './styles';
 
 type OrangeButtonProps = RectButtonProperties & {
   children: string;
+  isLoading?: boolean;
 };
 
-const OrangeButton: React.FC<OrangeButtonProps> = ({ children, ...rest }) => {
+const OrangeButton: React.FC<OrangeButtonProps> = ({
+  children,
+  isLoading = false,
+  ...rest
+}) => {
+  const { colors } = useContext(ThemeContext);
   return (
     <S.Container {...rest}>
       <S.ContainerBg>
-        <S.ContainerText>{children}</S.ContainerText>
+        {isLoading ? (
+          <ActivityIndicator size={20} color={colors.white} />
+        ) : (
+          <S.ContainerText>{children}</S.ContainerText>
+        )}
       </S.ContainerBg>
     </S.Container>
   );
