94fc17f4bc1c7c934002fd2f312800266e330b7d
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-11-24 00:43:38 -0200

    🐛  fix: inputs dont clear

--- mobile/src/components/Inputs/Input/index.tsx
+++ mobile/src/components/Inputs/Input/index.tsx
@@ -4,6 +4,7 @@ import React, {
   forwardRef,
   useCallback,
   useState,
+  useImperativeHandle,
 } from 'react';
 import { TextInput, TextInputProps } from 'react-native';
 
@@ -25,7 +26,11 @@ type InputValueRef = {
   value: string;
 };
 
-const Input: React.ForwardRefRenderFunction<TextInput, InputProps> = (
+type InputElementRef = {
+  focus: () => void;
+};
+
+const Input: React.ForwardRefRenderFunction<InputElementRef, InputProps> = (
   { name, label, hasNext = true, onChangeText, rawValue = '', ...rest },
   ref,
 ) => {
@@ -34,6 +39,8 @@ const Input: React.ForwardRefRenderFunction<TextInput, InputProps> = (
   const inputValueRef = useRef<InputValueRef>({
     value: defaultValue,
   });
+
+  const inputElementRef = useRef<TextInput>(null);
 
   const [isFocused, setIsFocused] = useState(false);
 
@@ -45,6 +52,12 @@ const Input: React.ForwardRefRenderFunction<TextInput, InputProps> = (
     [onChangeText],
   );
 
+  useImperativeHandle(ref, () => ({
+    focus: () => {
+      inputElementRef.current?.focus();
+    },
+  }));
+
   useEffect(() => {
     registerField<string>({
       name: fieldName,
@@ -52,6 +65,11 @@ const Input: React.ForwardRefRenderFunction<TextInput, InputProps> = (
 
       getValue: input => {
         return rawValue || input.value;
+      },
+
+      clearValue: (_, newValue) => {
+        inputElementRef.current?.clear();
+        inputValueRef.current.value = newValue;
       },
     });
   }, [registerField, fieldName, rawValue]);
@@ -62,7 +80,7 @@ const Input: React.ForwardRefRenderFunction<TextInput, InputProps> = (
 
       <S.InputWrapper>
         <S.Input
-          ref={ref}
+          ref={inputElementRef}
           defaultValue={defaultValue}
           onChangeText={handleOnChangeText}
           onFocus={() => setIsFocused(true)}
