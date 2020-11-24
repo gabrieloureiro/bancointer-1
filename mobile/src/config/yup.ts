cc878c1f86dca80227634eec399515c1de24fdfd
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-11-24 00:19:59 -0200

    ⚡  chore: add new default messages to yup

--- mobile/src/config/yup.ts
+++ mobile/src/config/yup.ts
@@ -4,4 +4,8 @@ export default setLocale({
   mixed: {
     required: 'Campo obrigatório',
   },
+  string: {
+    min: ({ min }) => `Mínimo de ${min} dígitos`,
+    max: ({ max }) => `Máximo de ${max} dígitos`,
+  },
 });
